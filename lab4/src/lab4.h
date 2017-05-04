9a7665950ba39638d4baf4b77cb768971ba0f959
MrP4p3r <mrp4p3r@gmail.com>
2017-05-04 16:33:25 +0300

    Добавлены файлы для ЛР4

---
+++ lab4/src/lab4.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////
+// lab4.h
+
+#include "pr_a.h"
+
+#include "pr_b.h"
+
+#ifndef DO_A
+#define DO_A(X) pr_a(X);
+
+#endif /*DO_A*/
+
+#ifndef DO_B
+#define DO_B(X) pr_b(X);
+
+#endif /*DO_B*/
